290103030bdf2e422566b613859e521b7a109d76
김태윤 <xodbs1758@gmail.com>
2025-08-17 16:56:01 +0900

    08/17 MainContent title 위치수정

--- src/molecules/TitleLayer.jsx
+++ src/molecules/TitleLayer.jsx
@@ -8,7 +8,7 @@ const TitleLayer = ({ before, after }) => {
       <section className="h-max relative bg-transparent flex flex-col justify-center items-center gap-2">
         <div className="relative flex justify-center items-center">
           <VideoBox className="absolute top-0 left-0 w-full h-full object-cover" />
-          <div className="absolute flex flex-col gap-3 justify-center items-center">
+          <div className="absolute top-64 flex flex-col gap-3 justify-center items-center">
             <Title init={before} ani={after} />
             <SubTitle init={before} ani={after} />
           </div>
